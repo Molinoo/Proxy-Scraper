2882963cfcbac0fa10e4cae10f5bbbfce6f63aff
Molinoo <30395348+Molinoo@users.noreply.github.com>
2017-07-30 01:10:11 +0100

    Proxy and Error count added

--- Proxy_Scraper.py
+++ Proxy_Scraper.py
@@ -1,3 +1,6 @@
+#Coded using Python 3.5
+#Developed for Secrecy
+
 import requests
 import sys
 import re
@@ -9,26 +12,35 @@ proxy_count = 0
 
 def dg(line):
     if debug==True:
-        print "[Debug]" + line
+        print("[Debug]" + line)
+
+def Stats():
+    dg('[+]All websites scraped.\n')
+    dg('[+]Gathered a total of ' + str(proxy_count) + ' proxies.')
+    dg('[+]Encountered a total of ' + str(error_count) + ' errors.')
 
 
 def Source_1():
+    global error_count
+    global proxy_count
     url_list = {'https://free-proxy-list.net/anonymous-proxy.html',
                 'https://free-proxy-list.net/uk-proxy.html',
                 'https://free-proxy-list.net/',
-                'https://www.us-proxy.org/'}
+                'https://www.us-proxy.org/'}    
     for url in url_list:
+        dg('[+]Scraping from ' + url + '.')
         is_online = False
-        dg('Testing URL')
+        dg('[+]Testing URL')
         try:
             #url = "https://free-proxy-list.net/anonymous-proxy.html"
             r = requests.get(url)
             is_online = True
-            dg('URL is online')
+            dg('[+]URL is online')
         except:
-            dg('URL not found')
+            error_count += 1
+            dg('[-]URL not found')
 
-        dg('Writting to file')
+        dg('[+]Writting to file')
         if is_online == True:
             for td in range(1,80):
                 proxies = open('proxies.txt', 'a+')
@@ -38,16 +50,20 @@ def Source_1():
                     port = tree.xpath('//*[@id="proxylisttable"]/tbody/tr[' + str(td) + ']/td[2]/text()')
                     entry = ip[0] + ':' + port[0] + '\n'
                     proxies.write(entry)
+                    proxy_count += 1
                     proxies.close
                 except:
-                    dg('Current XPath not found')
+                    dg('[-]Current XPath not found')
+                    error_count += 1
                     proxies.close
-
+                    
+        dg('[+]Proxies written to file.')
 
 def main():
-    dg('Starting scraper')
-    #dg('Scraping from https://free-proxy-list.net/anonymous-proxy.html')
+    dg('[+]Starting scraper')
     Source_1()
+
+    Stats()
     
 
 if __name__=="__main__":
